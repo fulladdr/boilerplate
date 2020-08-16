d307554aed250113c22d41de509832376e6ac662
fulladdr <57664064+fulladdr@users.noreply.github.com>
2020-08-16 12:46:56 +0900

    second commit

--- README.txt
+++ README.txt
@@ -2,3 +2,6 @@
 2. npm install express --save
 --save is form storing information
 every dependencies are in node_module
+
+
+3. npm install mongoose --save
--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express')//express 모듈을 가져옴
 const app = express() //새로운 express app을 만듬
 const port = 5000   //port number은 마음대로 설정 가능
 
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://fulladdr:<980402>@fulladdr.id96y.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false//to prevent error
+}).then()
+
+
 app.get('/', (req, res) => {//루트 디렉토리로 가면 hello world 출력 가능
     res.send('Hello World!')
 })
