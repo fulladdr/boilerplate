adc019998d0e67aead8a851ecffbc3e3828a5977
fulladdr <57664064+fulladdr@users.noreply.github.com>
2020-09-07 10:31:31 +0900

    minor changes to files

--- server/config/key.js
+++ server/config/key.js
@@ -1,6 +1,7 @@
 //local일 때와 production일때를 차이
 if (process.env.NODE_ENV === 'production') {
     module.exports = require('./prod');
-} else {
+} 
+else {
     module.exports = require('./dev');
-}P
+}
