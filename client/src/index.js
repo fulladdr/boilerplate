278f57e2a2b8ac5a2627cdeb7145204ea1cced4e
fulladdr <57664064+fulladdr@users.noreply.github.com>
2020-09-10 21:17:07 +0900

    redux connected

--- client/src/index.js
+++ client/src/index.js
@@ -8,15 +8,23 @@ import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
 import promiseMiddleware from 'redux-promise';
 import ReduxThunk from 'redux-thunk';
+import Reducer from "./_reducer";
 
 const createStoreWithMiddleware = applyMiddleware(promiseMiddleware,ReduxThunk)(createStore)
-
 ReactDOM.render(
   <Provider
-  store={}>
+    store={createStoreWithMiddleware(
+      Reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+  >
+    <App />
+  </Provider>,
 
-  </Provider>
+  document.getElementById("root")
 );
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
